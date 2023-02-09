0e19d388179cf73491e08d2a6440eb1681a10c43
Aitor Rodriguez <Aitor.Gallardo.14@gmail.com>
2023-02-09 15:46:23 +0100

    initial commit

--- src/main/java/com/aitor/pruebaspring/MiControlador.java
+++ src/main/java/com/aitor/pruebaspring/MiControlador.java
@@ -1,2 +1,16 @@
-package com.aitor.pruebaspring;public class MiControlador {
+package com.aitor.pruebaspring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDateTime;
+
+@Controller
+public class MiControlador {
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("hora", LocalDateTime.now());
+        return "index";
+    }
 }
